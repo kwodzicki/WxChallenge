9302f8783c39f6f7071866b96dcaf0ce76ed286b
kwodzicki <krwodzicki@gmail.com>
2018-09-24 16:22:27 -0500

    Updates setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ import setuptools
 
 
 setuptools.setup(
-  name         = "WxChallenge",
-  description  = "For parsing WxChallenge data for grading",
-  url          = "https://github.com/kwodzicki/WxChallenge",
-  author       = "Kyle R. Wodzicki",
-  author_email = "krwodzicki@gmail.com",
-  version      = "0.1.1",
-  packages     = setuptools.find_packages(),
+  name             = "WxChallenge",
+  description      = "For parsing WxChallenge data for grading",
+  url              = "https://github.com/kwodzicki/WxChallenge",
+  author           = "Kyle R. Wodzicki",
+  author_email     = "krwodzicki@gmail.com",
+  version          = "0.1.2",
+  packages         = setuptools.find_packages(),
   install_requires = [ "bs4", "numpy", "pandas", "xlwt" ],
+  scripts          = ['bin/WxChall_Daily_Update'],
   zip_safe = False
 );
