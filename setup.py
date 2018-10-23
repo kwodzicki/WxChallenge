82b28a16a94a3b76bfa2aafe3f9398d49d71f490
kwodzicki <krwodzicki@gmail.com>
2018-10-23 17:09:08 -0500

    Adds WxChall.sql file to setup.py so is removed on uninstall

--- setup.py
+++ setup.py
@@ -4,16 +4,18 @@ import setuptools
 
 
 setuptools.setup(
-  name             = "WxChallenge",
-  description      = "For parsing WxChallenge data for grading",
-  url              = "https://github.com/kwodzicki/WxChallenge",
-  author           = "Kyle R. Wodzicki",
-  author_email     = "krwodzicki@gmail.com",
-  version          = "0.2.4",
-  packages         = setuptools.find_packages(),
-  install_requires = [ "bs4", "lxml", "numpy", "pandas", "openpyxl" ],
-  scripts          = ['bin/WxChall_Daily_Update',
-                      'bin/WxChall_Semester_Update',
-                      'bin/WxChall_Grades'],
-  zip_safe = False
+  name                 = "WxChallenge",
+  description          = "For parsing WxChallenge data for grading",
+  url                  = "https://github.com/kwodzicki/WxChallenge",
+  author               = "Kyle R. Wodzicki",
+  author_email         = "krwodzicki@gmail.com",
+  version              = "0.2.4",
+  packages             = setuptools.find_packages(),
+  install_requires     = [ "bs4", "lxml", "numpy", "pandas", "openpyxl" ],
+  scripts              = ['bin/WxChall_Daily_Update',
+                          'bin/WxChall_Semester_Update',
+                          'bin/WxChall_Grades'],
+  package_data         = {'WxChallenge' : ['WxChall.sql']},
+  include_package_date = True,
+  zip_safe             = False,
 );
