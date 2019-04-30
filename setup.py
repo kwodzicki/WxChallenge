c751fedcda66c48d566dc92b025335033478c7ca
Kyle Wodzicki <wodzicki@tamu.edu>
2019-04-30 10:15:32 -0500

    Moves version number to version.py
    The verion number is now in the version.py file as __version__.
    This version number is also imported into the CLIs so that it
    can be displayed at run time using the --version flag
    
    v0.2.11

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
-from setuptools import setup
 import setuptools
+from distutils.util import convert_path
 
-
+main_ns  = {};
+ver_path = convert_path("video_utils/version.py");
+with open(ver_path) as ver_file:
+  exec(ver_file.read(), main_ns);
+  
 setuptools.setup(
   name                 = "WxChallenge",
   description          = "For parsing WxChallenge data for grading",
   url                  = "https://github.com/kwodzicki/WxChallenge",
   author               = "Kyle R. Wodzicki",
   author_email         = "krwodzicki@gmail.com",
-  version              = "0.2.10",
+  version              = main_ns['__version__'],
   packages             = setuptools.find_packages(),
   install_requires     = [ "bs4", "lxml", "numpy", "scipy", "pandas", "openpyxl" ],
   scripts              = ['bin/WxChall_Daily_Update',
