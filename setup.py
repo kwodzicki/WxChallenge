b2666ba10bde4ac14188d3eec3ece8cddb6b77f3
Kyle Wodzicki <wodzicki@tamu.edu>
2019-05-01 15:50:12 -0500

    Cleans up code in CLIs
    Argument adding to the argparser in the CLIs is formatted to be
    more readable; each keyword given own line.
    
    Added commented out code in setup.py that will enable moving
    of .sql file to home directory on update and then moving file
    back to correct location on install. This is likely not needed
    though because .sql file is not shipped with package. and is
    therefore not touch on update
    
    v0.2.16

--- setup.py
+++ setup.py
@@ -7,11 +7,45 @@ from distutils.util import convert_path
 pkg_name = "WxChallenge";
 pkg_desc = "For parsing WxChallenge data for grading";
 pkg_url  = "https://github.com/kwodzicki/WxChallenge";
+sqlFile  = "WxChall.sql"
+userHome = os.path.expanduser('~');
 
 main_ns  = {};
 ver_path = convert_path("{}/version.py".format(pkg_name));
 with open(ver_path) as ver_file:
   exec(ver_file.read(), main_ns);
+
+'''
+sys.path.pop(0);                                                                # Pop off current directory from path
+pkg_info = importlib.util.find_spec( pkg_name );                                # Look for the package; may be installed
+if pkg_info:                                                                    # If the package is found
+  pkg_dir = os.path.dirname( pkg_info.origin );                                 # Current package directory
+  sqlPath = os.path.join( pkg_dir, sqlFile );                                   # Get current .sql file location
+  if os.path.isfile( sqlPath ):                                                 # If the SQL file exists
+    os.rename( sqlPath, os.path.join( userHome, sqlFile ) );                    # Move the file to the user's home directory
+
+class CustomInstall( install ):
+  def _post_install( self ):
+    '''
+    Purpose:
+      Post-installation method for keeping config settings
+    Inputs:
+      None
+    '''
+    def find_module_path():
+      '''Local function for determining package installation directory'''
+      for p in sys.path:                                                        # Iterate over all system paths
+        if os.path.isdir(p) and pkg_name in os.listdir(p):                      # If the path is a directory and the package name is in the directory list
+          print(p, pkg_name)
+          return os.path.join(p, pkg_name);                                     # Return path to package
+    install_path = find_module_path();                                          # Find the package path
+    sqlPath = os.path.join( userHome, sqlFile );                                # Path of backed up SQL file
+    if os.path.isfile( sqlPath ):                                               # If the temprorary config file exists
+      os.rename( sqlPath, os.path.join( install_path, sqlFile ) );              # Move file to package location
+  def run( self ):
+    install.run( self );
+    self._post_install();
+'''
   
 setup(
   name                 = pkg_name,
@@ -25,7 +59,7 @@ setup(
   scripts              = ['bin/WxChall_Daily_Update',
                           'bin/WxChall_Semester_Update',
                           'bin/WxChall_Grades'],
-  package_data         = {'WxChallenge' : ['WxChall.sql']},
+  package_data         = {'WxChallenge' : [ sqlFile ]},
   include_package_date = True,
   zip_safe             = False,
 );
